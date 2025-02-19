8ae06981cd0fb85aeb5f8561fb039bfaf7867545
Pedro Luis Tapia <99137141+PedroTapia98@users.noreply.github.com>
2025-02-19 17:42:22 -0600

    Agregar amigos al array

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let NoAmigos = [];
+let ListaAmigos = [];
 
-function verificarValor(){
-    NoAmigos.push(document.getElementById('amigo').value);
+function agregarAmigo(){
+    //Obtener el valor del input 
+    let NombreAmigo = document.getElementById("amigo");
+    let ValAmigo = input.value.trim();//Para poder eliminar lo posibles espaciós al inicio o al final de input
+
+    //Asegurarnos de no tener valores nulos
+    if (ValAmigo  === ""){
+        alert("Ingrese un valor valido");
+        return;
+    }
+
+    //Agregar el valor al Array
+    ListaAmigos.push(ValAmigo);
+
+    console.log("Lista de amigos:", ListaAmigos);
         
 }
